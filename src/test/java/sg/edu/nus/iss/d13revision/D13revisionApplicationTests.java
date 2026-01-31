e012f11e674a4f79c5d0a1649f6991cf24f24730
Kwok <kwok_leong1985@hotmail.com>
2026-01-31 13:29:20 +0800

    Resetting repo to subfolder level

---
+++ src/test/java/sg/edu/nus/iss/d13revision/D13revisionApplicationTests.java
@@ -0,0 +1,40 @@
+package sg.edu.nus.iss.d13revision;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import sg.edu.nus.iss.d13revision.controllers.DataController;
+
+@SpringBootTest
+class D13revisionApplicationTests {
+
+	@Autowired
+	DataController dataController;
+
+	@Test
+	void health() throws Exception {
+		assertEquals("HEALTH CHECK OK!", dataController.healthCheck());;
+	}
+
+	@Test
+	void version() {
+	assertEquals("The actual version is 1.0.0", dataController.version());
+	}
+
+	@Test
+	void nationsLength() {
+	Integer nationsLength = dataController.getRandomNations().size();
+	assertEquals(10, nationsLength);
+	}
+	
+	@Test
+	void currenciesLength() {
+	Integer currenciesLength = dataController.getRandomCurrencies().size();
+	assertEquals(20, currenciesLength);
+	}
+
+
+}
